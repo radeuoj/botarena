e9ced9a1ebbb896bea536189d37b23f276d01d54
Florin MOTOTOLEA <victor.020030@gmail.com>
2026-05-10 13:31:46 +0300

    am schimbat culorile

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@ struct TestBot : Bot {
 		turn(glm::radians(5.0));
 	}
 };
+struct TestBot2 : Bot {
+	void update() {
+		go(10);
+		turn(glm::radians(5.0));
+	}
+};
 
 int main() {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
@@ -18,18 +24,21 @@ int main() {
 
 	TestBot test_bot;
 	test_bot.position = { 300, 300 };
+	TestBot2 test_bot2;
+	test_bot2.position = { 450, 300 };
 	LuaBot lua_bot("luabots/test.lua");
 
 	Arena arena;
 	arena.bots.push_back(&test_bot);
 	arena.bots.push_back(&lua_bot);
+	arena.bots.push_back(&test_bot2);
 	arena.init();
 	
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		
-		ClearBackground(RAYWHITE);
-		DrawText(std::format("SALUT! FPS: {}", 1.0 / GetFrameTime()).c_str(), 100, 200, 20, BLACK);
+		ClearBackground(BLACK);
+		DrawText(std::format("SALUT! FPS: {}", 1.0 / GetFrameTime()).c_str(), 100, 200, 20, RED);
 		arena.try_update_and_draw();
 
 		EndDrawing();
